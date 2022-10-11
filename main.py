b2b664307c31af96c92ad3bf7d4b62608f2a2ce0
TitouanUniLu <titouan.guerin.001@student.uni.lu>
2022-10-11 15:37:53 +0200

    first batch of results

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import pandas
 import re
 import time
+import csv
 
 #CREATING A LIST OF ALL WEBSITE FROM THE XLSX FILE
 workbook = pandas.read_excel('ALL_INDICES-2021.xlsx')
@@ -12,8 +13,9 @@ website_list = df['Company website'].tolist()
 board_web_list = df['Website Board of Directors'].tolist()
 all_regular_expressions = df.columns[2:len(df.columns)-4].tolist()
 
+
 for i in range(0, len(all_regular_expressions)-1):
-    all_regular_expressions[i] = all_regular_expressions[i].replace("+OR+", "|")
+    all_regular_expressions[i] = all_regular_expressions[i].replace("OR", "|")
     all_regular_expressions[i] = all_regular_expressions[i].replace("+", " ") #is plus a space in the expression??
     all_regular_expressions[i] = all_regular_expressions[i].replace("%22", "")
     #until index 23 we don't have AND
@@ -42,22 +44,35 @@ def text_from_html(body):
     visible_texts = filter(tag_visible, texts) #second function call, to check if text is visible 
     return u" ".join(t.strip() for t in visible_texts)  #.strip() removes extra spaces
 
+file = open('results.csv', 'w')
+writer = csv.writer(file)
+
 #get word to look for
-def mainLoop(list):
+def mainLoop(list, regex_list, file):
     start_time=time.time()
+    header = ["Website Name"]
+    for elem in regex_list: header.append(elem)
+    writer.writerow(header)
     for website in list:
         print("\nWebsite number", list.index(website)+1, "scraped: ", website)
         try:
-            print("error: ", requests.get(website).raise_for_status())
+            data = []
+            data.append(website)
+            error_msg = "error: " + str(requests.get(website).raise_for_status())
+            print(error_msg)
             html_request = requests.get(website).text
 
             #get all text from wepage (and lowercase it)
             all_html_text = text_from_html(html_request).lower()
 
             #find all occurences of word usign RE
-            for regex in temp_regex:
+            for regex in regex_list:
                 matches = re.findall(re.compile(regex), all_html_text)
                 print(regex+":", len(matches))
+                data.append(len(matches))
+            #ex for garmin.com, i get results (which are true) unlike the results in all_indices.xlsx
+            writer.writerow(data)
+            
             
         except Exception as e:
             print("error: ", e) 
@@ -67,4 +82,4 @@ def mainLoop(list):
 
 if __name__ == "__main__":
     print("-- STARTING THE PROGRAM --")
-    mainLoop(website_list)
+    mainLoop(website_list, temp_regex, file)
